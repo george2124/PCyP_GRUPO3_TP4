e402f031ba4f2b42b9a0b272d29411e16480be14
jorge apaza <jorgeapaza2124@gmail.com>
2026-09-24 21:47:25 -0300

    primer commit del tp4

---
+++ src/main/java/ar/edu/unju/fi/poo/PcypTp4Grupo3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcypTp4Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcypTp4Grupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/poo/PcypTp4Grupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcypTp4Grupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
